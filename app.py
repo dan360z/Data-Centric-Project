e5997f1f02da580b4752ae266337cf3010b4d7c6
Daniel Field <dannyf506@googlemail.com>
2019-06-16 14:21:39 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'RecipeBook'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.Recipes.find())
+    
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', '5000')),
+            debug=True)    
+
