a56e4c178d2fe10ccc785f9bd3ea32edf0c070fa
Daniel Field <dannyf506@googlemail.com>
2019-06-22 16:25:01 +0100

    Implemented add your own recipe functionality via a form in addrecipe.html and styled

--- app.py
+++ app.py
@@ -10,22 +10,54 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
-'''This function displays all the recipes on the landing page'''
+'''This displays all the recipes on the landing page'''
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.Recipes.find())
 
+'''This displays a filtered view of the recipes by the course the user has selected'''
 @app.route('/get_course/<course>')
 def get_course(course):
     recipes = mongo.db.Recipes.find({"course_name": course})
     return render_template("recipes.html", recipes=recipes)    
 
-'''This function dispalys the full recipe of which the user has clicked'''
+'''This dispalys the full recipe of which the user has clicked'''
 @app.route('/show_recipe/<recipe_id>')
 def show_recipe(recipe_id):
     this_recipe = mongo.db.Recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("fullrecipe.html", recipe=this_recipe)
+
+'''This renders an add recipe form'''    
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html", courses=mongo.db.Course.find(), cuisines=mongo.db.Cuisine.find())
+
+'''This takes the add recipe form data and inserts it in to the Recipe collection as a document in MongoDB
+then redirects back to recipes.html to view the newly added recipe. prep and cook times are added together to return a total cook time.'''
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    
+    ingredients = request.form["ingredients"].split(",")
+    method = request.form["method"].split(",")
+    
+    int_prep = int(request.form["prep"])
+    int_cook = int(request.form["cook"])
+    int_total_time = int_prep + int_cook
+    total_time = str(int_total_time)
+    
+    mongo.db.Recipes.insert_one({'name': request.form["name"],
+                                'author': request.form["author"],
+                                'cuisine_name': request.form["cuisine_name"],
+                                'course_name': request.form["course_name"],
+                                'cals': request.form["cals"],
+                                'prep': request.form["prep"],
+                                'cook': request.form["cook"],
+                                'total_time': total_time,
+                                'ingredients': ingredients,
+                                'method': method})
+
+    return redirect(url_for('get_recipes'))
  
     
 if __name__ == '__main__':
