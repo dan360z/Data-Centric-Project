1772836ed7df5c6c00e716a795f2b7964af91fb3
Daniel Field <dannyf506@googlemail.com>
2019-07-20 16:21:10 +0100

    Put comments inside functions for easier reading in app.py and added sort recipesby likes iin ascending order

--- app.py
+++ app.py
@@ -10,66 +10,71 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
-'''This displays all the recipes on the landing page'''
+
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
+    '''This displays all the recipes on the landing page'''
     recipes_count = mongo.db.Recipes.count()
     return render_template("recipes.html", recipes=mongo.db.Recipes.find(), courses=mongo.db.Course.find(), recipes_count=recipes_count)
 
 
-'''This sorts the recipes displayed in acending order alphabetically'''
 @app.route('/sort_az')
 def sort_az():
+    '''This sorts the recipes displayed in acending order alphabetically'''
     recipes_count = mongo.db.Recipes.count()
     return render_template("recipes.html", recipes=mongo.db.Recipes.find().sort("name", 1), courses=mongo.db.Course.find(), recipes_count=recipes_count)
 
 
-'''This sorts the recipes displayed by the ammount of likes each recipe has in decending order'''
-@app.route('/sort_likes')
-def sort_likes():
+@app.route('/sort_likes_ascending')
+def sort_likes_ascending():
+    '''This sorts the recipes displayed by the ammount of likes each recipe has in ascending order'''
+    recipes_count = mongo.db.Recipes.count()
+    return render_template("recipes.html", recipes=mongo.db.Recipes.find().sort("likes", 1), courses=mongo.db.Course.find(), recipes_count=recipes_count)
+
+@app.route('/sort_likes_descending')
+def sort_likes_descending():
+    '''This sorts the recipes displayed by the ammount of likes each recipe has in decending order'''
     recipes_count = mongo.db.Recipes.count()
     return render_template("recipes.html", recipes=mongo.db.Recipes.find().sort("likes", -1), courses=mongo.db.Course.find(), recipes_count=recipes_count)
 
 
-'''This displays a filtered view of the recipes by the course the user has selected'''
 @app.route('/get_course/<course>')
 def get_course(course):
+    '''This displays a filtered view of the recipes by the course the user has selected'''
     recipes = mongo.db.Recipes.find({"course_name": course})
     recipes_count = mongo.db.Recipes.count({"course_name": course})
 
     return render_template("recipes.html", recipes=recipes, courses=mongo.db.Course.find(), recipes_count=recipes_count)    
 
 
-'''This dispalys the full recipe of which the user has clicked'''
 @app.route('/show_recipe/<recipe_id>')
 def show_recipe(recipe_id):
+    '''This dispalys the full recipe of which the user has clicked'''
     this_recipe = mongo.db.Recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("fullrecipe.html", recipe=this_recipe, icon="favorite_border")
 
 
-'''This increments the likes for the recipe by one each time a user clicks on the like icon'''
 @app.route('/like/<recipe_id>')
 def like(recipe_id):
-
+    '''This increments the likes for the recipe by one each time a user clicks on the like icon'''
     mongo.db.Recipes.update({'_id': ObjectId(recipe_id)},
                             { "$inc": {'likes': 1} })
 
     this_recipe = mongo.db.Recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("fullrecipe.html", recipe=this_recipe, icon="favorite")    
 
-
-'''This returns an add recipe form'''    
+    
 @app.route('/add_recipe')
 def add_recipe():
+    '''This returns an add recipe form'''
     return render_template("addrecipe.html", courses=mongo.db.Course.find(), cuisines=mongo.db.Cuisine.find())
 
 
-'''This takes the add recipe form data and inserts it in to the Recipe collection as a document in MongoDB
-then redirects back to recipes.html to view the newly added recipe. prep and cook times are added together to return a total cook time.'''
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-    
+    '''This takes the add recipe form data and inserts it in to the Recipe collection as a document in MongoDB
+    then redirects back to recipes.html to view the newly added recipe. prep and cook times are added together to return a total cook time.'''
     ingredients = request.form["ingredients"].split("\n")
     method = request.form["method"].split("\n")
     
@@ -93,18 +98,17 @@ def insert_recipe():
     return redirect(url_for('get_recipes'))
 
 
-'''This returns an add recipe form'''
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
+    '''This returns an add recipe form'''
     this_recipe = mongo.db.Recipes.find_one({"_id": ObjectId(recipe_id)})
 
     return render_template("editrecipe.html", recipe=this_recipe, courses=mongo.db.Course.find(), cuisines=mongo.db.Cuisine.find())
 
 
-'''This updates an existing recipe'''
 @app.route('/update_recipe/<recipe_id>', methods=['POST'])
 def update_recipe(recipe_id):
-
+    '''This updates an existing recipe'''
     ingredients = request.form["ingredients"].split("\n")
     method = request.form["method"].split("\n")
         
@@ -130,9 +134,9 @@ def update_recipe(recipe_id):
     return render_template("fullrecipe.html", recipe=this_recipe)
 
 
-'''This removes a recipe from the database'''
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
+    '''This removes a recipe from the database'''
     mongo.db.Recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_recipes'))
 
