63022d931662c7d3348d3c7596def7591e254593
Daniel Field <dannyf506@googlemail.com>
2019-07-23 22:02:26 +0100

    Added a warning modal when a user clicks the delete recipe button for confirmation

--- static/js/components.js
+++ static/js/components.js
@@ -1,7 +1,8 @@
-//This function is for the dropdown menus and nav bar
 
+//These functions are for the dropdown menus, nav bar and modal
 $(document).ready(function () {
     $('.button-collapse').sideNav();
     $('select').material_select();
     $('.dropdown-trigger').dropdown();
+    $('.modal').modal();
 });
